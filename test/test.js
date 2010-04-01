43cd0707a10023c45bd778e70748fedb2cdca146
Tim Caswell <tim@creationix.com>
2010-04-01 12:26:09 -0500

    Add initial git library to help with the document database.

---
+++ test/test.js
@@ -0,0 +1,5 @@
+var Wheat = require(__dirname + '/../lib/wheat');
+var sys = require('sys');
+sys.error(sys.inspect(Wheat))
+Wheat("/Users/tim/Code/howtonode.org")
+Wheat("/Users/tim/git/howtonode.org.git")
