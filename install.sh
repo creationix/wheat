13dc8119bb3d972fc84713d5e8fbb2ef5d5825db
Tim Caswell <tim@creationix.com>
2010-04-06 11:19:06 -0500

    Fix the install script so it actually works and add some instructions for users.

--- install.sh
+++ install.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+echo
+echo "Loading dependencies as git submodules..."
+git submodule init
+git submodule update
+echo
+echo "Creating ~/.node_libraries folder if it doesn't exist yet..."
 mkdir -p ~/.node_libraries
+echo
+echo "Linking current working copy into the .node_libraries folder..."
 ln -sf `pwd`/lib/wheat.js ~/.node_libraries/wheat.js
-ln -sf `pwd`/lib/wheat ~/.node_libraries/wheat
+rm ~/.node_libraries/wheat
+ln -s `pwd`/lib/wheat ~/.node_libraries/wheat
+echo
+echo "Done, if you delete this current working directory `pwd`, you will lose your library."
+echo "You can move it to a permanent place and re-run install if desired."
+echo
+echo "Enjoy blogging with Wheat!"
+echo
