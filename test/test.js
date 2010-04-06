a4ec951f410b46b6ada12732343358638820062a
Tim Caswell <tim@creationix.com>
2010-04-06 10:26:02 -0500

    Move the real test code to the test.js file.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var Wheat = require(__dirname + '/../lib/wheat');
 var sys = require('sys');
-sys.error(sys.inspect(Wheat))
-Wheat("/Users/tim/Code/howtonode.org")
-Wheat("/Users/tim/git/howtonode.org.git")
+
+// Test it!
+Wheat("/Users/tim/Code/howtonode.org", 8080);
